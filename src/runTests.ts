4748d7321bcb8d09590b5bea281f5ab5cf6428c2
John Eisenmann <esperantanaso@gmail.com>
2022-06-25 09:29:00 -0400

    Parse test suites

--- src/runTests.ts
+++ src/runTests.ts
@@ -14,9 +14,16 @@ enum PacketType {
     QuitProcess = 7,
 }
 
+enum FileType {
+    Generic = 0,
+    BytecodeApp = 1,
+    SystemApp = 2,
+}
+
 const currentDirectoryPath = pathUtils.dirname(fileURLToPath(import.meta.url));
 const projectDirectoryPath = pathUtils.dirname(currentDirectoryPath);
 const socketPath = pathUtils.join(projectDirectoryPath, "testSocket");
+const testSuitesDirectoryPath = pathUtils.join(projectDirectoryPath, "testSuites");
 const packetHeaderSize = 5;
 
 let socketClient: net.Socket;
@@ -52,6 +59,39 @@ class Packet {
             socketClient.write(this.body);
         }
     };
+}
+
+class Test {
+    name: string;
+    files: TestFile[];
+    expectedValues: number[];
+    
+    constructor(name: string) {
+        this.name = name;
+        this.files = [];
+        this.expectedValues = [];
+    }
+}
+
+abstract class TestFile {
+    name: string;
+    type: FileType;
+    isGuarded: boolean;
+    lines: string[];
+    
+    constructor(name: string, type: FileType, isGuarded: boolean) {
+        this.name = name;
+        this.type = type;
+        this.isGuarded = isGuarded;
+        this.lines = [];
+    }
+}
+
+class BytecodeFile extends TestFile {
+    
+    constructor(name: string, isGuarded: boolean) {
+        super(name, FileType.BytecodeApp, isGuarded);
+    }
 }
 
 const handleSocketData = (buffer: Buffer): void => {
@@ -106,27 +146,82 @@ const createSocket = (): Promise<net.Server> => new Promise(async (resolve, reje
     console.log("Waiting for WheatSystem to start...");
 });
 
-const runTest = async (): Promise<void> => {
-    while (true) {
-        const outputPacket = new Packet(PacketType.ResetState);
-        console.log("Sending packet to test socket: " + outputPacket.toString());
-        outputPacket.send();
-        const inputPacket = await receivePacket();
-        console.log("Received packet from test socket: " + inputPacket.toString());
-        await new Promise((resolve, reject) => {
-            setTimeout(resolve, 1000);
-        });
-    };
+const trimEnd = (text: string): string => {
+    let endIndex = text.length;
+    while (endIndex > 0) {
+        if (text.charAt(endIndex - 1) !== " ") {
+            break;
+        }
+        endIndex -= 1;
+    }
+    return text.substring(0, endIndex);
 };
 
-const runTests = async (): Promise<void> => {
-    const socketServer = await createSocket();
-    await runTest();
-    console.log("Closing test socket...");
-    await socketServer.close();
+const isSeparator = (text: string): boolean => {
+    if (text.length < 5) {
+        return false;
+    }
+    for (let index = 0; index < text.length; index++) {
+        if (text.charAt(index) !== "=") {
+            return false;
+        }
+    }
+    return true;
+};
+
+const parseTests = (lines: string[]): Test[] => {
+    const output: Test[] = [];
+    let currentTest: Test = null;
+    let currentFile: TestFile = null;
+    for (const untrimmedLine of lines) {
+        const line = trimEnd(untrimmedLine);
+        if (line.startsWith(">>> ")) {
+            currentFile = null;
+            const terms = line.substring(4, line.length).split(" ");
+            const command = terms[0];
+            const args = terms.slice(1, terms.length);
+            if (command === "TEST") {
+                currentTest = new Test(args[0]);
+                output.push(currentTest);
+            } else if (command === "BYTECODE_FILE") {
+                const isGuarded = (parseInt(args[1], 10) !== 0);
+                currentFile = new BytecodeFile(args[0], isGuarded);
+                currentTest.files.push(currentFile);
+            } else if (command === "EXPECT") {
+                args.forEach((arg) => {
+                    const value = parseInt(arg, 10);
+                    currentTest.expectedValues.push(value);
+                });
+            }
+        } else if (isSeparator(line)) {
+            currentTest = null;
+            currentFile = null;
+        } else if (line.length > 0) {
+            currentFile.lines.push(line);
+        }
+    }
+    return output;
+}
+
+const runTestSuite = async (path: string): Promise<void> => {
+    const lines = fs.readFileSync(path, "utf8").split("\n");
+    const tests = parseTests(lines);
+    // TODO: Run the tests.
+    
+};
+
+const runTestSuites = async (): Promise<void> => {
+    //const socketServer = await createSocket();
+    const fileNames = fs.readdirSync(testSuitesDirectoryPath);
+    for (const fileName of fileNames) {
+        const filePath = pathUtils.join(testSuitesDirectoryPath, fileName);
+        await runTestSuite(filePath);
+    }
+    //console.log("Closing test socket...");
+    //await socketServer.close();
     console.log("Finished running tests.");
 };
 
-runTests();
+runTestSuites();
 
 
